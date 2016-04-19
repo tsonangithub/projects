93a65cb68b0ff5fff1370d13f1e72dc442747468
tsonan <tsonan.github@gmail.com>
2016-04-19 11:26:28 +0300

    improvements

--- api-gateway/src/main/java/com/example/ReservationApiGatewayRestController.java
+++ api-gateway/src/main/java/com/example/ReservationApiGatewayRestController.java
@@ -6,31 +6,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
-//@RequestMapping("/")
+// @RequestMapping("/")
 public class ReservationApiGatewayRestController {
 
 	@Autowired
 	@LoadBalanced
 	private RestTemplate restTemplate;
 
-//	ParameterizedTypeReference<Resources<Reservation>> ptr = new ParameterizedTypeReference<Resources<Reservation>>() {
-//	};
-	
+	// ParameterizedTypeReference<Resources<Reservation>> ptr = new
+	// ParameterizedTypeReference<Resources<Reservation>>() {
+	// };
+
+	@CrossOrigin
 	@HystrixCommand(fallbackMethod = "getRandomStringFallback")
 	@RequestMapping("/random")
 	public String getRandomString() {
-		 ResponseEntity<String> responseEntity = this.restTemplate.exchange("http://random-words-service/word", HttpMethod.GET, null, String.class);
+		ResponseEntity<String> responseEntity = this.restTemplate.exchange("http://random-words-service/word",
+				HttpMethod.GET, null, String.class);
 		return responseEntity.getBody();
 	}
-	
+
 	public String getRandomStringFallback() {
 		return UUID.randomUUID().toString();
 	}
+
+	@CrossOrigin
+	@HystrixCommand(fallbackMethod = "concatenateFallback")
+	@RequestMapping("/concatenate/{inputString}")
+	public String concatenate(@PathVariable String inputString, @RequestParam String otherString) {
+		String url = "http://random-words-service/concat/" + inputString + "?otherString=" + otherString;
+		ResponseEntity<String> responseEntity = this.restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+		return responseEntity.getBody();
+	}
+
+	public String concatenateFallback() {
+		return "concatanation is unavailable right now";
+	}
+
+	@CrossOrigin
+	@HystrixCommand(fallbackMethod = "substringFallback")
+	@RequestMapping("/substring/{inputString}")
+	public String substring(@PathVariable String inputString, @RequestParam int start, @RequestParam int end) {
+		String url = "http://random-words-service/substring/" + inputString + "?start=" + start + "&end=" + end;
+		ResponseEntity<String> responseEntity = this.restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+		return responseEntity.getBody();
+	}
+
+	public String substringFallback() {
+		return "substring is unavailable right now";
+	}
+
 }
